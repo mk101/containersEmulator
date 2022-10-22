bead6749ff1ae209b5d15e9c9e8ed41a9c49648e
Maxim Kolesov <pro100maks222@gmail.com>
2022-10-22 10:43:31 +0300

    EmulatorService is working

--- emulator/src/main/java/com/example/emulator/Main.java
+++ emulator/src/main/java/com/example/emulator/Main.java
@@ -1,7 +1,25 @@
 package com.example.emulator;
 
+import com.example.emulator.data.EmulatorConfig;
+import com.example.emulator.service.EmulatorService;
+import com.example.emulator.service.GsonParserService;
+import com.example.emulator.service.ParserService;
+import com.example.emulator.service.SendService;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        ParserService parser = new GsonParserService();
+        SendService sendService = System.out::println;
+        EmulatorConfig config = null;
+        try {
+            config = parser.getEmulatorConfig("emulator/config.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        EmulatorService emulator = new EmulatorService(config, parser, sendService);
+        emulator.start();
     }
 }
--- emulator/src/main/java/com/example/emulator/service/EmulatorService.java
+++ emulator/src/main/java/com/example/emulator/service/EmulatorService.java
@@ -1,17 +1,45 @@
 package com.example.emulator.service;
 
+import com.example.emulator.data.ContainerConfig;
 import com.example.emulator.data.EmulatorConfig;
+import com.example.emulator.runnable.ContainerRunnable;
 import lombok.Getter;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class EmulatorService {
     @Getter
     private final EmulatorConfig config;
+    private final ParserService parserService;
+    private final SendService sendService;
 
-    public EmulatorService(EmulatorConfig config) {
+    public EmulatorService(EmulatorConfig config, ParserService parserService, SendService sendService) {
         this.config = config;
+        this.parserService = parserService;
+        this.sendService = sendService;
     }
 
     public void start() {
+        Calendar time = new GregorianCalendar();
+        time.setTime(config.getStartEmulatingTime());
 
+        for (int i = 0; i < config.getContainersCount(); i++) {
+            ContainerConfig containerConfig = ContainerConfig.builder()
+                    .containerNumber(i+1)
+                    .startEmulatingTime(time)
+                    .maxTimeShift(config.getMaxTimeShift())
+                    .recordingStep(config.getRecordingStep())
+                    .stepCount(config.getStepCount())
+                    .sensors(config.getSensors())
+                    .sendInterval(config.getSendInterval())
+                    .build();
+
+            Thread thread = new Thread(new ContainerRunnable(
+                containerConfig, parserService, sendService
+            ));
+
+            thread.start();
+        }
     }
 }
