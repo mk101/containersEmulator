41d1d9f995dbabb8c4f62f68c9bd62d7d5b4a26d
Maxim Kolesov <pro100maks222@gmail.com>
2022-12-04 12:18:46 +0300

    Add logger to processor

--- processor/src/main/java/com/example/processor/listener/ContainerListener.java
+++ processor/src/main/java/com/example/processor/listener/ContainerListener.java
@@ -3,18 +3,20 @@ package com.example.processor.listener;
 import com.example.processor.dto.ContainerDto;
 import com.example.processor.service.ContainerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ContainerListener {
 
     private final ContainerService containerService;
 
     @KafkaListener(containerFactory = "kafkaListenerContainerFactory", topics = "containers")
     public void onMessage(ContainerDto containerDto) {
-        System.out.println(containerService.save(containerDto).toString()); //TODO: add logger
+        log.info("New message from kafka: {}", containerService.save(containerDto).toString());
     }
 }
